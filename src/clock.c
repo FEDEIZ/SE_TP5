e143eca3062f27da5d8f15f012e967c474ebe235
FEDEIZ <ing.finterlandi@gmail.com>
2022-12-06 19:29:16 -0300

    test 1 al 6 completos!

--- src/clock.c
+++ src/clock.c
@@ -4,6 +4,15 @@
 #define SIZE_TIME 6
 #define INIT_VALUE 0
 #define SEC_UNITS 5
+#define SEC_TENS 4
+#define MIN_UNITS 3
+#define MIN_TENS 2
+#define HOURS_UNITS 1
+#define HOURS_TENS 0
+#define MAX_TENS_VALUES 6
+#define MAX_UNITS_VALUES 10
+#define MAX_HOURS_TENS_VALUE 2  // 24 hrs
+#define MAX_HOURS_UNITS_VALUE 4 // 24 hrs
 
 struct clock_s{
     bool valid;
@@ -14,36 +23,90 @@ struct clock_s{
 
 static struct clock_s instances;
 
+static const uint8_t BCD_LIMITS[] = {MAX_TENS_VALUES, MAX_UNITS_VALUES};
+
+void IncrementTime(clock_t clock){
+    for(int index = sizeof(clock->time) - 1; index>=0; index--){
+        clock->time[index]++;
+
+        if(clock->time[index]> BCD_LIMITS[index%2]){
+            clock->time[index] = INIT_VALUE;
+        } else {
+            break;
+        }
+    }
+    // if(clock->time[HOURS_TENS] == MAX_HOURS_TENS_VALUE){
+    //   if(clock->time[HOURS_UNITS] == MAX_HOURS_UNITS_VALUE){
+    //     clock->time[HOURS_TENS] = INIT_VALUE;
+    //     clock->time[HOURS_UNITS] = INIT_VALUE;
+    //   }  
+    // }
+}
+
 //pongo la hora en cero
 clock_t createClock(uint16_t ticksPerSec){
-    return NULL;
-    // instances.valid = false;
-    // instances.ticksPerSec = ticksPerSec;
-    // instances.ticksCount =0;
-    // memset(instances.time, INIT_VALUE, SIZE_TIME);
-    // return &instances;
+    instances.valid = false;
+    instances.ticksPerSec = ticksPerSec;
+    instances.ticksCount = INIT_VALUE;
+    memset(instances.time, INIT_VALUE, SIZE_TIME);
+    return &instances;
 }
 
 bool getTimeClock(clock_t clock, uint8_t * time, uint8_t size){
-    memset(time,0,size);
-    return false;
-    // memcpy(time, clock->time, size);
-    // return clock->valid;
+    memcpy(time,clock->time,  size);
+    return clock->valid;
+    
+    
 }
 
 void configClock(clock_t clock, uint8_t const * const time, uint8_t size){
-    memcpy(clock->time, time, size);
-    clock->valid = true;
+    
+     memcpy( clock->time,time, size);
+     clock->valid = true;
 }
 
 void tickClock(clock_t clock){
     clock->ticksCount++;
-    if(clock->ticksCount == clock->ticksPerSec){
-        clock->ticksCount =0;
-        clock->time[SEC_UNITS]++;//los segundos estan en el 5to lugar del arreglo
+    
+     if(clock->ticksCount == clock->ticksPerSec){
+        
+         clock->ticksCount = INIT_VALUE;
+     //    IncrementTime(clock); no me funciona
+
+      clock->time[SEC_UNITS]++;  //los segundos estan en el 5to lugar del arreglo       
         if (clock->time[SEC_UNITS] == 10){
             clock->time[SEC_UNITS] = 0;
-            clock->time[4]++;
-        }
-    }
+            clock->time[SEC_TENS]++;
+            
+            if(clock->time[SEC_TENS] == 6){
+                clock->time[SEC_UNITS] = 0;
+                clock->time[SEC_TENS] = 0;
+                clock->time[MIN_UNITS]++;
+
+                if(clock->time[MIN_UNITS] == 10){
+                    clock->time[SEC_UNITS] = 0;
+                    clock->time[SEC_TENS] = 0;
+                    clock->time[MIN_UNITS]= 0;
+                    clock->time[MIN_TENS]++;
+
+                    if(clock->time[MIN_TENS] == 6){
+                        clock->time[SEC_UNITS] = 0;
+                        clock->time[SEC_TENS] = 0;
+                        clock->time[MIN_UNITS]= 0;
+                        clock->time[MIN_TENS] = 0;
+                        clock->time[HOURS_UNITS]++;
+
+                        if(clock->time[HOURS_UNITS] == 10){
+                            clock->time[SEC_UNITS] = 0;
+                            clock->time[SEC_TENS] = 0;
+                            clock->time[MIN_UNITS]= 0;
+                            clock->time[MIN_TENS] = 0;
+                            clock->time[HOURS_UNITS]=0;
+                            clock->time[HOURS_TENS]++;
+                        }
+                    }
+                }
+            }
+        }  
+     }
 }
--- test/test_reloj.c
+++ test/test_reloj.c
@@ -1,12 +1,15 @@
 #include "unity.h"
 #include "clock.h"
+
+#define TICKS_PER_SECOND 5
+
+clock_t clock;
 /*
-1) Configurar la biblioteca, consultar la hora y tiene que ser invalida.
-2) Configurar la biblioteca, ajustar la hora (con valores correctos), consultar la hora y tiene que ser valida.
+
+
 3) Configurar la biblioteca, ajustar la hora (con valores incorrectos) tiene devolver un error y al consultar la hora tiene que ser invalida.
-4) Simular el paso de n ciclos de reloj, consultar la hora y verificar que avanzo un segundo
-5) Simular el paso de 10*n ciclos de reloj, consultar la hora y verificar que avanzo diez segundos
-6) Simular el paso de un minuto, diez minutos, una hora, diez horas y un día completo.
+
+
 7) Configurar la hora de la alarma (con valores correctos) y revisar si la guarda.
 8) Configurar la hora de la alarma (con valores incorrectos) y revisar si la rechaza.
 9) Configurar la hora de la alarma (con valores correctos) y revisar si la queda activada.
@@ -19,10 +22,91 @@
 17) Si la alarma esta inactiva no debe sonar cuando la hora del reloj coincida con la hora de la alarma
 */
 
+void setUp(void){  // iniciamos un solo reloj para todas las pruebas. Nombre reservado para ceedling. Se ejecuta antes de cada prueba
+    static const uint8_t INITIAL[] = {1,2,3,4};
+    clock = createClock(TICKS_PER_SECOND);
+    configClock(clock, INITIAL, sizeof(INITIAL));
+}
+
+void simulateSeconds(int seconds){
+    for(int index=0; index < seconds * TICKS_PER_SECOND; index++){
+        tickClock(clock);
+    }
+}
+
+// 1) Configurar la biblioteca, consultar la hora y tiene que ser invalida.
+
 void test_initial_hour_invalid(void){
     static const uint8_t EXPECTED[] = {0,0,0,0,0,0}; 
-    clock_t clock = createClock(5);
     uint8_t hour[6];
-    TEST_ASSERT_FALSE(getTimeClock(clock, hour, 6));
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED,hour, 6);
+    clock = createClock(TICKS_PER_SECOND); // necesitamos que el reloj NO este en hora!
+    TEST_ASSERT_FALSE(getTimeClock(clock, hour, sizeof(hour)));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED,hour, sizeof(EXPECTED));
 }
+
+// 2) Configurar la biblioteca, ajustar la hora (con valores correctos), consultar la hora y tiene que ser valida.
+
+void test_set_up_current_time(void){
+    static const uint8_t INITIAL[] = {1,2,3,4};
+    static const uint8_t EXPECTED[] = {1,2,3,4,0,0};
+    uint8_t hour[6];
+    TEST_ASSERT_TRUE(getTimeClock(clock,hour,sizeof(hour)));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED, hour, sizeof(EXPECTED));
+
+}
+
+// 4) Simular el paso de n ciclos de reloj, consultar la hora y verificar que avanzo un segundo
+
+void test_one_second_elapsed(void){
+    static const uint8_t EXPECTED[] = {1,2,3,4,0,1};
+    uint8_t hour[6];
+    simulateSeconds(1);
+    getTimeClock(clock,hour,sizeof(hour));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED, hour, sizeof(EXPECTED));
+}
+
+
+// 5) Simular el paso de 10*n ciclos de reloj, consultar la hora y verificar que avanzo diez segundos
+
+void test_ten_second_elapsed(void){
+    static const uint8_t EXPECTED[] = {1,2,3,4,1,0};
+    uint8_t hour[6];
+    simulateSeconds(10);
+    getTimeClock(clock,hour,sizeof(hour));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED, hour, sizeof(EXPECTED));
+}
+
+// 6) Simular el paso de un minuto, diez minutos, una hora, diez horas y un día completo.
+
+void test_one_minute_elapsed(void){
+    static const uint8_t EXPECTED[] = {1,2,3,5,0,0};
+    uint8_t hour[6];
+    simulateSeconds(60);
+    getTimeClock(clock,hour,sizeof(hour));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED, hour, sizeof(EXPECTED));
+}
+
+void test_ten_minute_elapsed(void){
+    static const uint8_t EXPECTED[] = {1,2,4,4,0,0};
+    uint8_t hour[6];
+    simulateSeconds(600);
+    getTimeClock(clock,hour,sizeof(hour));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED, hour, sizeof(EXPECTED));
+}
+
+void test_one_hour_elapsed(void){
+    static const uint8_t EXPECTED[] = {1,3,3,4,0,0};
+    uint8_t hour[6];
+    simulateSeconds(3600);
+    getTimeClock(clock,hour,sizeof(hour));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED, hour, sizeof(EXPECTED));
+}
+
+void test_ten_hour_elapsed(void){
+    static const uint8_t EXPECTED[] = {2,2,3,4,0,0};
+    uint8_t hour[6];
+    simulateSeconds(36000);
+    getTimeClock(clock,hour,sizeof(hour));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(EXPECTED, hour, sizeof(EXPECTED));
+}
+
